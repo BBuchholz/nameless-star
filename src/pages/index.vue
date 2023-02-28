c07f0fea96620afe8acdc35f5788d13e0188c4aa
Brent Buchholz <brentbuchholz@gmail.com>
2023-02-28 10:14:52 -0600

    Mod template, experimental

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,18 +16,32 @@ const { t } = useI18n()
 
 <template>
   <div>
-    <div text-4xl>
+    <!-- <div text-4xl>
       <div i-carbon-campsite inline-block />
-    </div>
+    </div> -->
+    <!-- <div>
+      <img src="./sonic-circle-pic.jpg" alt="image of musicians around a fire">
+    </div> -->
     <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
+      <a rel="noreferrer" href="https://github.com/BBuchholz/nameless-star" target="_blank">
+        Sonic Circle
       </a>
     </p>
     <p>
       <em text-sm opacity-75>{{ t('intro.desc') }}</em>
     </p>
-
+    <p>---</p>
+    <p>
+      What's next?
+    </p>
+    <p>---</p>
+    <p>FUCK WITH THE STYLING (make all this prettier, I'm thinking IdeaComponent.vue or something to abstract the component)</p>
+    <p>---</p>
+    <p>ensure uuids on all ideas and have a vote button that pre-populates an email sent to circle admins when a user votes for an idea</p>
+    <p>---</p>
+    <p>setup "rounds" wherein you have anywhere between 1 and 4 Pomodoro sessions to do something to a mix and submit within a week, with your logged sessions to give credit for pragmatic expediency on the one hand and also patient devotion in the other, in this way participants earn placements within vector spaces of correlation (through commonality of medium)</p>
+    <p>---</p>
+    <p>use kits as first round starters (eg. "build a loop with this kit to be used by another participant in the next round(you will be swapping so try to keep it rhythmic :) )</p>
     <div py-4 />
 
     <TheInput
@@ -47,6 +61,9 @@ const { t } = useI18n()
         {{ t('button.go') }}
       </button>
     </div>
+    <div>
+      <img src="./sonic-circle-pic.jpg" alt="image of musicians around a fire">
+    </div>
   </div>
 </template>
 
@@ -54,3 +71,9 @@ const { t } = useI18n()
 meta:
   layout: home
 </route>
+
+<style>
+ul {
+  list-style: circle;
+}
+</style>
